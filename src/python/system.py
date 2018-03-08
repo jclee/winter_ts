05b3cc4acda6f7cb9b0d43ae6e0d8528c16480ca
Joe Lee <joe@jotaro.com>
2018-03-07 22:22:44 -0800

    WIP Reenable menu

--- src/python/system.py
+++ src/python/system.py
@@ -25,26 +25,25 @@ def mainTask():
 
     # TODO: Reenable
     #yield from intro.introTask()
-    #
-    #while True:
-    #    sound.fader.kill()
-    #    introMusic.position = 0
-    #    introMusic.Play()
-    #    # Workaround for Brython "yield from" expression bugs:
-    #    resultRef = [None]
-    #    yield from intro.menuTask(resultRef)
+    
+    while True:
+        sound.fader.kill()
+        introMusic.position = 0
+        introMusic.Play()
+        # Workaround for Brython "yield from" expression bugs:
+        resultRef = [None]
+        yield from intro.menuTask(resultRef)
 
-    #    if resultRef[0] == 0:
-    #        introMusic.Pause()
-    #        yield from engineObj.beginNewGameTask()
-    #    elif resultRef[0] == 1:
-    #        introMusic.Pause()
-    #        yield from engineObj.loadGameTask()
-    #    elif resultRef[0] == 2:
-    #        break
-    #    else:
-    #        assert False, 'Wacky intro menu result %i! :o' % resultRef[0]
-    yield from engineObj.beginNewGameTask()
+        if resultRef[0] == 0:
+            introMusic.Pause()
+            yield from engineObj.beginNewGameTask()
+        elif resultRef[0] == 1:
+            introMusic.Pause()
+            yield from engineObj.loadGameTask()
+        elif resultRef[0] == 2:
+            break
+        else:
+            assert False, 'Wacky intro menu result %i! :o' % resultRef[0]
 
     ika.Exit()
 
