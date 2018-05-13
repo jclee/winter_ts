f092f3ffc58270d046d01bc7b8d39d29907dde0e
Joe Lee <joe@jotaro.com>
2018-05-13 11:32:17 -0700

    Remove unused function

--- src/python/maps/map12.py
+++ src/python/maps/map12.py
@@ -1,9 +1,2 @@
 def to11(engineRef):
     yield from engineRef.mapSwitchTask('map11.ika-map', (23 * 16, 17 * 16))
-
-# Unused?
-def heal(engineRef):
-    engineRef.player.stats.hp = 999
-    engineRef.player.stats.mp = 999
-    if False:
-        yield None
